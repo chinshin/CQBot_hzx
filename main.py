c45295f60662462b4b06fd20da89e8b5c4c56673
ChenZhen <imlex@live.cn>
2018-02-19 21:03:13 +0800

    Improve

--- main.py
+++ main.py
@@ -35,86 +35,83 @@ interval_kd = kd_interval()
 
 def getModian():
     # bot = CQHttp(api_root='http://127.0.0.1:5700/')
-    while True:
-        try:
-            # INFO(printStrTime() + 'check modian')
-            INFO('check modian')
-            stampTime = int(time.time())
-            msgDict = newOrder(stampTime, int(interval_md))
-            if msgDict:
-                for msg in msgDict['msg']:
-                    msg += msgDict['end']
-                    # print(printStrTime() + msg)
-                    bot.send_group_msg_async(group_id=groupid(), message=msg, auto_escape=False)
-                    time.sleep(0.1)
-        except Exception as e:
-            WARN('error when getModian')
-        finally:
-            # INFO(printStrTime() + 'modian check completed')
-            INFO('modian check completed')
+    try:
+        # INFO(printStrTime() + 'check modian')
+        INFO('check modian')
+        stampTime = int(time.time())
+        msgDict = newOrder(stampTime, int(interval_md))
+        if msgDict:
+            for msg in msgDict['msg']:
+                msg += msgDict['end']
+                # print(printStrTime() + msg)
+                bot.send_group_msg_async(group_id=groupid(), message=msg, auto_escape=False)
+                time.sleep(0.1)
+    except Exception as e:
+        WARN('error when getModian')
+    finally:
+        # INFO(printStrTime() + 'modian check completed')
+        INFO('modian check completed')
 
 
 def getWeibo():
     # bot = CQHttp(api_root='http://127.0.0.1:5700/')
-    while True:
-        try:
-            # INFO(printStrTime() + 'check weibo')
-            INFO('check weibo')
-            global weibo_id_array
-            global firstcheck_weibo
-            wbcontent = ''
-            idcount = -1
-            if (firstcheck_weibo == 1):
-                # INFO(printStrTime() + 'first check weibo')
-                INFO('first check weibo')
-                weibo_id_array = copy.copy(getidarray())
-                firstcheck_weibo = False
-            checkwbid = copy.copy(get_5_idarray())
-            if (firstcheck_weibo == 0):
-                for cardid in checkwbid:
-                    idcount += 1
-                    if int(cardid) == 0:
-                        continue
-                    if cardid not in weibo_id_array:
-                        weibo_id_array.append(cardid)
-                        retweet = checkretweet(idcount)
-                        wbpic = checkpic(idcount)
-                        wbscheme = getscheme(idcount)
-                        if (retweet):
-                            wbcontent = "小偶像刚刚[转发]了一条微博：" + '\n' + '\n' + getretweetweibo(idcount) + '\n'
-                            wbcontent = wbcontent + '\n' + "传送门：" + wbscheme
-                        else:
-                            wbcontent = "小偶像刚刚发了一条新微博：" + '\n' + '\n' + getweibo(idcount) + '\n'
-                            if (wbpic):
-                                wbcontent = wbcontent + getpic(idcount)
-                            wbcontent = wbcontent + '\n' + "传送门：" + wbscheme
-                        # print(printStrTime() + wbcontent)
-                        bot.send_group_msg_async(group_id=groupid(), message=wbcontent, auto_escape=False)
-        except Exception as e:
-            WARN('error when getWeibo')
-        finally:
-            # INFO(printStrTime() + 'weibo check completed')
-            INFO('weibo check completed')
+    try:
+        # INFO(printStrTime() + 'check weibo')
+        INFO('check weibo')
+        global weibo_id_array
+        global firstcheck_weibo
+        wbcontent = ''
+        idcount = -1
+        if (firstcheck_weibo == 1):
+            # INFO(printStrTime() + 'first check weibo')
+            INFO('first check weibo')
+            weibo_id_array = copy.copy(getidarray())
+            firstcheck_weibo = False
+        checkwbid = copy.copy(get_5_idarray())
+        if (firstcheck_weibo == 0):
+            for cardid in checkwbid:
+                idcount += 1
+                if int(cardid) == 0:
+                    continue
+                if cardid not in weibo_id_array:
+                    weibo_id_array.append(cardid)
+                    retweet = checkretweet(idcount)
+                    wbpic = checkpic(idcount)
+                    wbscheme = getscheme(idcount)
+                    if (retweet):
+                        wbcontent = "小偶像刚刚[转发]了一条微博：" + '\n' + '\n' + getretweetweibo(idcount) + '\n'
+                        wbcontent = wbcontent + '\n' + "传送门：" + wbscheme
+                    else:
+                        wbcontent = "小偶像刚刚发了一条新微博：" + '\n' + '\n' + getweibo(idcount) + '\n'
+                        if (wbpic):
+                            wbcontent = wbcontent + getpic(idcount)
+                        wbcontent = wbcontent + '\n' + "传送门：" + wbscheme
+                    # print(printStrTime() + wbcontent)
+                    bot.send_group_msg_async(group_id=groupid(), message=wbcontent, auto_escape=False)
+    except Exception as e:
+        WARN('error when getWeibo')
+    finally:
+        # INFO(printStrTime() + 'weibo check completed')
+        INFO('weibo check completed')
 
 
 def getRoomMsg():
     # bot = CQHttp(api_root='http://127.0.0.1:5700/')
-    while True:
-        try:
-            INFO('check room')
-            msg_array = roomMsg()
-            if msg_array:
-                INFO('new room msg')
-                msg_array.reverse()
-                for msgdata in msg_array:
-                    bot.send_group_msg_async(group_id=groupid(), message=msgdata, auto_escape=False)
-        except Exception as e:
-            # raise e
-            WARN('error when getRoomMsg')
-        else:
-            pass
-        finally:
-            INFO('room-check completed')
+    try:
+        INFO('check room')
+        msg_array = roomMsg()
+        if msg_array:
+            INFO('new room msg')
+            msg_array.reverse()
+            for msgdata in msg_array:
+                bot.send_group_msg_async(group_id=groupid(), message=msgdata, auto_escape=False)
+    except Exception as e:
+        # raise e
+        WARN('error when getRoomMsg')
+    else:
+        pass
+    finally:
+        INFO('room-check completed')
 
 
 # 添加调度任务， 间隔为 0 则不添加
