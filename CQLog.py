abf40a0c7f9dbc0bcf830d0a3fd9d69ac84c5824
ChenZhen <imlex@live.cn>
2018-04-27 21:39:15 +0800

    log improve

--- CQLog.py
+++ CQLog.py
@@ -3,17 +3,18 @@ import logging.handlers
 import os
 
 BASE_DIR = os.path.dirname(__file__)
-LOG_FILE = os.path.join(BASE_DIR, 'tst.log')
+LOG_FILE = os.path.join(BASE_DIR, 'CQBot.log')
 # LOG_FILE = r'tst.log'
 
-handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=1024 * 1024, backupCount=5, encoding='utf-8')  # 实例化handler
+# handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=1024 * 1024, backupCount=5, encoding='utf-8')  # 实例化handler
+handler = logging.FileHandler(LOG_FILE)
 fmt = '%(asctime)s - %(levelname)s - %(message)s'
 # fmt = '%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s - %(message)s'
 
 formatter = logging.Formatter(fmt)  # 实例化formatter
 handler.setFormatter(formatter)  # 为handler添加formatter
 
-logger = logging.getLogger('tst')  # 获取名为tst的logger
+logger = logging.getLogger('CQBot')  # 获取名为tst的logger
 logger.addHandler(handler)  # 为logger添加handler
 logger.setLevel(logging.DEBUG)
 
